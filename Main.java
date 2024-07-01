95f65b7050ffa33c19218cff543c3055012b2daf
Alpha <81774909+alpha-Issagha@users.noreply.github.com>
2024-07-01 15:53:45 +0200

    rr

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello Alpha!");
+        System.out.println("Hello Alpha Comment tu vas ?");
     }
 }
